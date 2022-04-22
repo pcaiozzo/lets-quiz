933c4537f28ceeaf1643668cf42167048b71112d
pcaiozzo <paigecaiozzo@gmail.com>
2022-04-22 11:05:19 -0400

    added a few functions to js

---
+++ assets/js/game.js
@@ -0,0 +1,13 @@
+//Functions
+
+function makeQuiz(){}
+
+function showResults(){}
+
+//End of functions
+
+//display quiz right when page loads
+makeQuiz();
+
+// on submit, show results
+submitButton.addEventListener(' click ', showResults);
