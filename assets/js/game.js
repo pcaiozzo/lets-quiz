2601f2148763614f4169bfde781a911caceb647c
pcaiozzo <paigecaiozzo@gmail.com>
2022-04-22 11:15:15 -0400

    added questions section to js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,10 @@
+//references to html variables
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+
+
 //Functions
 
 function makeQuiz(){}
@@ -11,3 +18,57 @@ makeQuiz();
 
 // on submit, show results
 submitButton.addEventListener(' click ', showResults);
+
+//Questions section
+const myQuestions = [
+{
+  question: "question here",
+  answers: {
+    a: "one"
+    b: "two"
+    c: "three"
+  },
+  correctAnswer: "c"
+},
+
+{
+  question: "question here",
+  answers: {
+    a: "one"
+    b: "two"
+    c: "three"
+  },
+  correctAnswer: "c"
+},
+
+{
+  question: "question here",
+  answers: {
+    a: "one"
+    b: "two"
+    c: "three"
+  },
+  correctAnswer: "c"
+},
+
+{
+  question: "question here",
+  answers: {
+    a: "one"
+    b: "two"
+    c: "three"
+  },
+  correctAnswer: "c"
+},
+
+{
+  question: "question here",
+  answers: {
+    a: "one"
+    b: "two"
+    c: "three"
+  },
+  correctAnswer: "c"
+},
+
+];
