c2739e3b111bfb88f9db83f00f0ce159f4ba7216
pcaiozzo <paigecaiozzo@gmail.com>
2022-05-01 19:14:22 -0400

    added intro and timer

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,6 +6,8 @@ const answerButton2 = document.getElementById("answerButton2");
 const answerButton3 = document.getElementById("answerButton3");
 const answerButton4 = document.getElementById("answerButton4");
 const solutionAnswer = document.getElementById("solutionAnswer");
+const timerEl = document.querySelector("#timer");
+const introductionSection = document.getElementById("introduction")
 
 const myQuestions = {
   1: {
@@ -27,6 +29,7 @@ const myQuestions = {
 let currentQuestion = 1;
 let score = 0;
 let scores = [];
+let timeLeft = 75;
 
 function answerQuestion(choice) {
   if (choice === myQuestions[currentQuestion].answer) {
@@ -52,6 +55,7 @@ function populateQuestion() {
   answerButton2.innerHTML = myQuestions[currentQuestion].choices[1];
   answerButton3.innerHTML = myQuestions[currentQuestion].choices[2];
   answerButton4.innerHTML = myQuestions[currentQuestion].choices[3];
+  timerClock ();
 }
 
 function showAnswer() {
@@ -60,6 +64,24 @@ function showAnswer() {
 
 function hideStartQuizButton() {
   startQuizButton.style.display = "none";
+  introductionSection.style.display = "none";
+
+}
+
+function timerClock () {
+  var timer = setInterval(function (){
+    if(timeLeft <=0){
+      clearInterval(timer);
+      timerEl.innerHTML = "0";
+      endGame(timeLeft);
+    } else if (timeLeft === "done"){
+      clearInterval(timer);
+      timerEl.innerHTML = "0";
+    } else {
+      timerEl.innerHTML = timeLeft + "s";
+    }
+    timeLeft -= 1;
+  }, 1000);
 }
 
 startQuizButton.addEventListener("click", showAnswer);
